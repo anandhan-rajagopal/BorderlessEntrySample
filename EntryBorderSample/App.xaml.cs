f8458f54ab47ea75e3c1036b0655ffdbd469bda1
Anandhan <anandhan.rajagopal@syncfusion.com>
2023-05-03 19:23:14 +0530

    Added sample project

---
+++ EntryBorderSample/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Platform;
+
+namespace EntryBorderSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		Microsoft.Maui.Handlers.EntryHandler.Mapper.PrependToMapping(nameof(Entry), (handler, view) =>
+		{
+			if (view is Entry)
+			{
+	#if ANDROID
+				handler.PlatformView.SetBackgroundColor(Colors.Transparent.ToPlatform());
+	#elif IOS || MACCATALYST
+				handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+                
+	#endif
+			}
+		});
+
+		MainPage = new AppShell();
+	}
+}
---
+++ EntryBorderSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EntryBorderSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
